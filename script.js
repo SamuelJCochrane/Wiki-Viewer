ba37fae330145277dfaad155597119170308051c
SamuelJCochrane <32472108+SamuelJCochrane@users.noreply.github.com>
2017-10-12 17:24:13 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+var searchButton 		= document.getElementById('searchButton'),
+ 	searchBar 			= document.getElementById('searchBar'),
+ 	topBar 				= document.getElementById('topBar'),
+ 	resultsContainer	= document.getElementById('resultsContainer');
+
+
+function getResults(searchText){
+	$.ajax({
+		url: "https://en.wikipedia.org/w/api.php?action=opensearch&search="+searchText+"&limit=11&namespace=0&format=json",
+		jsonp: "callback",
+		dataType: "jsonp",
+		})
+			.done(function(response) {
+				var titlesArray = response[1].slice(1);
+				var descripArray = response[2].slice(1);
+				var linkArray = response[3].slice(1);
+				titlesArray.forEach(function(e, i) {
+					resultsContainer
+						.appendChild(document.createElement('div'))
+							.setAttribute('id', i)
+				var idiv = document.getElementById(i);
+				idiv.innerHTML = '<h3>'+e+'</h3><br><p>'+descripArray[i]+'</p>';
+				idiv.addEventListener('click', function(){window.open(linkArray[i], '_blank' )})
+				})
+			})
+}
+
+
+function activate(){
+	if (searchBar.value){
+		topBar.setAttribute('style', 'height:0px');
+		getResults(searchBar.value);};
+}
+
+searchButton.addEventListener('click', activate);
+searchBar.addEventListener('submit', activate)
